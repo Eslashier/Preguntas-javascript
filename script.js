5e024bb65c976fd5889cef3b8d0ea10ec209b22a
Eslashier <anfelov@gmail.com>
2022-04-22 12:17:17 -0500

    preguntas con dificultad y salida

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const api_url = "https://api.jsonbin.io/b/6261f493c5284e3115511492/1";
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const exitButton = document.getElementById('exit-btn')
-const answeraButton = document.getElementById('answera')
-const answerbButton = document.getElementById('answerb')
-const answercButton = document.getElementById('answerc')
-const answerdButton = document.getElementById('answerd')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+const startButton = document.getElementById("start-btn");
+const scoreButton = document.getElementById("score-btn");
+const restartButton = document.getElementById("restart-btn");
+const saveButton = document.getElementById("save-btn");
+const exitButton = document.getElementById("exit-btn");
+const answeraButton = document.getElementById("a");
+const answerbButton = document.getElementById("b");
+const answercButton = document.getElementById("c");
+const answerdButton = document.getElementById("d");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
 
-var dificultad = -1;  
+var dificultad = -1;
 
 async function getapi(url) {
   const response = await fetch(url);
@@ -19,8 +21,8 @@ async function getapi(url) {
 }
 
 function aumentarDificultad() {
-    dificultad++;
-    return dificultad;
+  dificultad++;
+  return dificultad;
 }
 
 async function misPreguntasPorNivel() {
@@ -42,22 +44,40 @@ async function misPreguntasPorNivel() {
 async function cargarPregunta() {
   const pregunta = await misPreguntasPorNivel();
   console.log(pregunta);
-    questionElement.innerText = pregunta.question;
-    answeraButton.innerText = pregunta.asnwera;
-    answerbButton.innerText = pregunta.asnwerb;
-    answercButton.innerText = pregunta.asnwerc;
-    answerdButton.innerText = pregunta.asnwerd;
-
+  questionElement.innerText = pregunta.question;
+  answeraButton.innerText = pregunta.asnwera;
+  answerbButton.innerText = pregunta.asnwerb;
+  answercButton.innerText = pregunta.asnwerc;
+  answerdButton.innerText = pregunta.asnwerd;
 }
 
 function startGame() {
-    startButton.classList.add('hide')
-    questionContainerElement.classList.remove('hide')
-    aumentarDificultad()
-    cargarPregunta()
-  }
+  startButton.classList.add("hide");
+  scoreButton.classList.add("hide")
+  restartButton.classList.add("hide")
+  exitButton.classList.remove("hide")
+  questionContainerElement.classList.remove("hide");
+  aumentarDificultad();
+  cargarPregunta();
+}
 
-async function ejecutar(){
-    aumentarDificultad()
-    cargarPregunta()
+async function ejecutar() {
+  aumentarDificultad();
+  cargarPregunta();
+}
+
+async function respuesta(clickID) {
+  const pregunta = await misPreguntasPorNivel();
+  const idboton = document.getElementById(clickID).id;
+
+  if (idboton == pregunta.correctanswer) {
+    aumentarDificultad();
+    cargarPregunta();
+  } else {
+    questionContainerElement.classList.add("hide");
+    scoreButton.classList.add("hide")
+    restartButton.classList.remove("hide")
+    saveButton.classList.remove("hide")
+    exitButton.classList.add("hide")
+  }
 }
