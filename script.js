f62ac5db6b75204611c1ef365eafb4f5aaaad8a5
Eslashier <anfelov@gmail.com>
2022-04-22 10:36:32 -0500

    preguntas en html === preguntas cargadas en html

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const api_url = "https://api.jsonbin.io/b/6261f493c5284e3115511492/1";
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const exitButton = document.getElementById('exit-btn')
+const answeraButton = document.getElementById('answera')
+const answerbButton = document.getElementById('answerb')
+const answercButton = document.getElementById('answerc')
+const answerdButton = document.getElementById('answerd')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
 var dificultad = -1;  
 
@@ -8,21 +18,19 @@ async function getapi(url) {
   return data.pregunta;
 }
 
-
-function calcularDificultad() {
-  dificultad++;
-  return dificultad
+function aumentarDificultad() {
+    dificultad++;
+    return dificultad;
 }
 
 async function misPreguntasPorNivel() {
   const preguntas = await getapi(api_url);
   let preguntasPorDificultad = [];
-  var dif = calcularDificultad();
 
   for (i = 0; i < preguntas.length; i++) {
     let dificultadarreglo = parseInt(preguntas[i].difficulty);
 
-    if (dificultadarreglo == dif) {
+    if (dificultadarreglo == dificultad) {
       preguntasPorDificultad.push(preguntas[i]);
     }
   }
@@ -34,12 +42,22 @@ async function misPreguntasPorNivel() {
 async function cargarPregunta() {
   const pregunta = await misPreguntasPorNivel();
   console.log(pregunta);
-
-    let table = `<tr><th>Pregunta</thd></tr>`
-    table += `<tr><td>${pregunta.question}</td></tr>`
-
-    document.getElementById("employees").innerHTML = table
+    questionElement.innerText = pregunta.question;
+    answeraButton.innerText = pregunta.asnwera;
+    answerbButton.innerText = pregunta.asnwerb;
+    answercButton.innerText = pregunta.asnwerc;
+    answerdButton.innerText = pregunta.asnwerd;
 
 }
 
-cargarPregunta()
+function startGame() {
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    aumentarDificultad()
+    cargarPregunta()
+  }
+
+async function ejecutar(){
+    aumentarDificultad()
+    cargarPregunta()
+}
